3bde19798103e599eb55b3d791e076882b40d437
Shehuna2 <arbamiyya@gmail.com>
2026-01-04 23:03:06 +0100

    Integrate Sepolia contract addresses for PropertyToken, PropertyCrowdfund, ChainRegistry

--- packages/frontend/src/config/contracts.config.ts
+++ packages/frontend/src/config/contracts.config.ts
@@ -6,9 +6,9 @@ export interface ContractAddresses {
 
 export const CONTRACT_ADDRESSES: { [chainId: number]: ContractAddresses } = {
   1: {
-    PropertyToken: '0x0000000000000000000000000000000000000000', // To be deployed
-    PropertyCrowdfund: '0x0000000000000000000000000000000000000000',
-    ChainRegistry: '0x0000000000000000000000000000000000000000',
+    PropertyToken: '0x24e580A700C2cE6a324A32b8a9f4f0d20EC5b020',
+    PropertyCrowdfund: '0x705ca8D85C32Cd4D6456bf59F0Ed2F5e358D8062',
+    ChainRegistry: '0xD368b35D0beaCe446E6e174D420DB2E65F6b2fE0',
   },
   8453: {
     PropertyToken: '0x0000000000000000000000000000000000000000',
